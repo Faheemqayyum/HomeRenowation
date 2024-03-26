283cd9c509d178ef85e0e85663b57612c65ae302
Muhammad Nouman <110379750+nomiwsd@users.noreply.github.com>
2024-03-26 22:46:38 +0500

    new client changes

--- appsrc/views.py
+++ appsrc/views.py
@@ -53,6 +53,10 @@ def RecentMembers(request):
   return  render(request, 'Admin/RecentMembers.html')
 def RecentWorkers(request):
   return  render(request, 'Admin/RecentWorkers.html')
+def RecentWorkersDetail(request):
+  return  render(request, 'Admin/RecentWorkerDetail.html')
+# def RecentMembersDetail(request):
+#   return  render(request, 'Admin/RecentMemberDetail.html')
 
 # Worker
 def WorkerDashboard(request):
@@ -68,10 +72,12 @@ def EditWorkerProfile(request):
 
 # Client
 def ClientDashboard(request):
-  return  render(request, 'Worker/Dashboard.html')
+  return  render(request, 'Client/Dashboard.html')
 def ShowJobs(request):
-  return  render(request, 'Worker/PostJobs.html')
+  return  render(request, 'Client/PostJob.html')
 def AddJob(request):
-  return  render(request, 'Worker/AddJob.html')
+  return  render(request, 'Client/AddJob.html')
 def ClientProfile(request):
-  return  render(request, 'Worker/Profile.html')
+  return  render(request, 'Client/Profile.html')
+def EditClientProfile(request):
+  return  render(request, 'Client/EditProfile.html')
